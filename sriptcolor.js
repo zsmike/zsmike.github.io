fd9d3dffc72a937851d00b899f150babd2273657
zsmike <zsofia.mike@gmail.com>
2018-10-28 18:31:26 +0100

    test

---
+++ sriptcolor.js
@@ -0,0 +1,36 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+//event = input
+
+function setGradient () {
+		body.style.background = "linear-gradient(to right, "
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+// color1.addEventListener("input", function () {
+// 	console.log(color1.value);
+// 	body.style.background = "linear-gradient(to right, "
+// 	+ color1.value 
+// 	+ ", " 
+// 	+ color2.value 
+// 	+ ")";
+// })
+
+// color2.addEventListener("input", function (c) {
+// 	console.log(color2.value);
+// 		body.style.background = "linear-gradient(to right, "
+// 	+ color1.value 
+// 	+ ", " 
+// 	+ color2.value 
+// 	+ ")";
+// })
+
+color1.addEventListener("input", setGradient);
+color2.addEventListener("input", setGradient);
